03e27cf17b75de834cbf69405448cc84eaa52868
zzuse <zzuse@outlook.com>
2024-10-02 23:47:37 -0500

    STL vector

--- README.md
+++ README.md
@@ -64,5 +64,6 @@ chapter 162. std::function II -- commit 95edbb87ae4d7f578d53c70ba83e4cdd6ba6c358
 chapter 163. std::bind I -- commit f2166cf33de6fb4ef9a5260af5fd7eadb7279ba5
 chapter 164. std::bind II -- commit f42a2e413ce4ec1bf8c053fb2b54d25a7c00a8d4
 chapter 165. std::bind III -- commit 5dc010f3b6398c86dce5d9c86d7b23c81a5f4473
-chapter 167. std::array
+chapter 167. std::array -- commit 1058ac0b90f469d13a56fe4661a47018dff36605
+chapter 168. std::vector
 ```
--- STL/main.cpp
+++ STL/main.cpp
@@ -1,8 +1,10 @@
 #include <array>
 #include <iostream>
+#include <vector>
 
-void array()
+void Array()
 {
+    std::cout << "Array()" << std::endl;
     std::array<int, 5> arr{3, 1, 8, 5, 9};
     for (int i = 0; i < arr.size(); ++i) {
         arr[i] = i;
@@ -14,8 +16,35 @@ void array()
     }
     std::cout << arr.data() << std::endl;
 }
+
+void Vector()
+{
+    std::cout << "Vector()" << std::endl;
+    std::vector<int> coll{1, 2, 3, 4};
+    for (int i = 0; i < 5; ++i) {
+        coll.push_back(i * 10);
+        std::cout << coll.size() << " ";
+    }
+    std::cout << std::endl;
+
+    coll[0] = 100;
+    for (int i = 0; i < coll.size(); ++i) {
+        std::cout << coll[i] << " ";
+    }
+    std::cout << std::endl;
+
+    coll.insert(coll.begin(), 700);
+    coll.erase(coll.end() - 5);
+    coll.pop_back();
+    auto iter = coll.begin();
+    while (iter != coll.end()) {
+        std::cout << *iter++ << " ";
+    }
+}
+
 int main()
 {
-    array();
+    Array();
+    Vector();
     return 0;
 }
