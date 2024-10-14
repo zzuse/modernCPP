d7102ffece4b215ab03d266375fc7f7258c67e85
zzuse <zzuse@outlook.com>
2024-10-13 23:56:22 -0500

    feature test macros

--- Core17Features/main.cpp
+++ Core17Features/main.cpp
@@ -1,5 +1,14 @@
 // https://isocpp.org/files/papers/p0636r0.html
 
+#ifdef __has_include
+#if __has_include(<filesystem>)
+#include <filesystem>
+namespace fs = std::filesystem;
+#else
+#include <experimental/filesystem>
+#endif
+#endif
+
 #include <iostream>
 
 [[deprecated("Use the new version instead")]] int* CreateIntArray(size_t size) { return new int[size]; }
@@ -13,10 +22,9 @@ template <typename T>
 namespace [[deprecated("DO NOT USE")]] A {
 }
 
-class [[deprecated("This class is replaced by NewTest class")]] Test{};
+class [[deprecated("This class is replaced by NewTest class")]] Test {};
 
-class [[nodiscard]] Number {
-};
+class [[nodiscard]] Number {};
 
 Number GetNumber(int x) { return Number{}; }
 
@@ -27,5 +35,8 @@ int main()
     using namespace A;
     CreateArray<int>(3);
     GetNumber(3);
+    fs::path p{"/mnt/data"};
+    std::cout << __cpp_inline_variables << std::endl;
+    std::cout << __cpp_capture_star_this << std::endl;
     return 0;
 }
