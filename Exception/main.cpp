103699b54b7f4a885bfc989d1dd8745b94656adb
zzuse <zzuse@outlook.com>
2024-11-15 23:12:00 -0600

    exception stack unwinding

--- Exception/main.cpp
+++ Exception/main.cpp
@@ -1,17 +1,40 @@
 
 #include <iostream>
+#include <memory>
+#include <vector>
+
+class Test {
+public:
+    Test() { std::cout << "Test()" << std::endl; }
+    ~Test() { std::cout << "~Test()" << std::endl; }
+};
 
 int NewProcessRecords(long long count)
 {
+    Test *t = new Test; // leaking memory
     if (count < 10) throw std::out_of_range("Count should be greater than 10");
     int *p = new int[count];
     std::cout << "Success new " << count << std::endl;
     delete[] p;
+    delete t;
+    return 0;
+}
+
+int ContainerProcessRecords(long long count)
+{
+    std::vector<long long> pArray;
+    pArray.reserve(count);
+    std::cout << "Success init vector " << count << std::endl;
+    for (long long i = 0; i < count; ++i) {
+        pArray.push_back(i);
+    }
     return 0;
 }
 
 int ProcessRecords(int count)
 {
+    Test t;
+    std::unique_ptr<Test> t2(new Test);
     if (count < 10) throw std::out_of_range("Count should be greater than 10");
     int *pArray = (int *)malloc(count * sizeof(int));
     if (pArray == nullptr) {
@@ -43,7 +66,13 @@ int main()
         } catch (std::runtime_error &ex) {
             std::cout << ex.what() << std::endl;
         }
+        try {
+            ContainerProcessRecords(std::numeric_limits<long long>::max());
+        } catch (std::bad_alloc &ex) {
+            std::cout << ex.what() << std::endl;
+        }
     } catch (std::exception &ex) {
+        std::cout << "What makes you here" << std::endl;
         std::cout << ex.what() << std::endl;
     } catch (...) {
         std::cout << "Exception" << std::endl;
