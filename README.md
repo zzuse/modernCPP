c1f40054c6e0b99589e3fe23f4457ed215179489
zzuse <zzuse@outlook.com>
2024-11-28 23:43:31 -0600

    parallel accumulate

--- README.md
+++ README.md
@@ -160,7 +160,7 @@ chapter 12. Thransfer ownership of a thread -- commit e0d9c2005c06ea197ee8d3cb7c
 chapter 13. Useful operation on thread -- commit aee84fe0a824a2be1bcaeb6d1f6100653c9ea5d1
 SailingCrew/
 chapter 14. Sail a ship with work queue -- commit 043e79eda6f7e71332270db75b24b4aaf68a5c81
-chapter 15. Parallel accumulate
+chapter 16. Parallel accumulate
 ```
 
 # 现代 C++ 编程实战
--- SailingCrew/main.cpp
+++ SailingCrew/main.cpp
@@ -140,9 +140,57 @@ void runAccumulate()
     std::cout << "dash fold : " << std::accumulate(v.begin(), v.end(), std::to_string(v[0]), dash_fold) << std::endl;
 }
 
+#define MIN_BLOCK_SIZE 1000
+
+template <typename iterator, typename T>
+T accumulate(iterator start, iterator end, T& ref)
+{
+    ref = std::accumulate(start, end, 0);
+    return ref;
+}
+
+template <typename iterator, typename T>
+T parallel_accumulate(iterator start, iterator end, T& ref)
+{
+    int input_size = std::distance(start, end);
+    int allowed_threads_by_elements = (input_size) / MIN_BLOCK_SIZE;
+    int allowed_threads_by_hardware = std::thread::hardware_concurrency();
+    int num_threads = std::min(allowed_threads_by_elements, allowed_threads_by_hardware);
+    int block_size = (input_size + 1) / num_threads;
+
+    std::vector<T> results(num_threads);
+    std::vector<std::thread> threads(num_threads - 1);
+
+    iterator last;
+    for (int i = 0; i < num_threads - 1; i++) {
+        last = start;
+        std::advance(last, block_size);
+        threads[i] = std::thread(accumulate<iterator, T>, start, last, std::ref(results[i]));
+        start = last;
+    }
+
+    results[num_threads - 1] = std::accumulate(start, end, 0);
+    std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
+    return std::accumulate(results.begin(), results.end(), ref);
+}
+
+void run_parallel_accumulate()
+{
+    const int size = 8000;
+    int* my_array = new int[size];
+    int ref = 0;
+    srand(std::time(0));
+    for (size_t i = 0; i < size; i++) {
+        my_array[i] = rand() % 10;
+    }
+    int ref_val = parallel_accumulate<int*, int>(my_array, my_array + size, ref);
+    std::cout << "Accumulated value: " << ref_val << std::endl;
+}
+
 int main()
 {
     runAccumulate();
+    run_parallel_accumulate();
 
     provideTheCommandCaptain();
     std::queue<bool> cq, eq;
