47ebe6e9ac940384b7f74ae2182490d20880c5ad
zzuse <zzuse@outlook.com>
2024-12-05 23:59:28 -0600

    basic stack

--- ThreadSafe/main.cpp
+++ ThreadSafe/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <list>
 #include <mutex>
+#include <stack>
 #include <thread>
 
 /*
@@ -83,9 +84,71 @@ void run_malisious_code()
     std::cout << unprotected_data->nothing << std::endl;
 }
 
+template <typename T>
+class trivial_thread_safe_stack {
+    std::stack<T> stk;
+    std::mutex m;
+
+public:
+    void push(T element)
+    {
+        std::lock_guard<std::mutex> lg(m);
+        stk.push(element);
+    }
+
+    void pop()
+    {
+        std::lock_guard<std::mutex> lg(m);
+        stk.pop();
+    }
+
+    T& top()
+    {
+        std::lock_guard<std::mutex> lg(m);
+        return stk.top();
+    }
+
+    bool empty()
+    {
+        std::lock_guard<std::mutex> lg(m);
+        return stk.empty();
+    }
+
+    size_t size()
+    {
+        std::lock_guard<std::mutex> lg(m);
+        return stk.size();
+    }
+};
+
+void run_trivial_thread_safe_stack()
+{
+    trivial_thread_safe_stack<int> stk{};
+    stk.push(1);
+    stk.push(2);
+    std::thread thread_1([&stk] {
+        if (!stk.empty()) {
+            int value = stk.top();
+            std::cout << "value --" << value << std::endl;
+            stk.pop();
+        }
+    });
+    std::thread thread_2([&stk] {
+        if (!stk.empty()) {
+            int value = stk.top();
+            std::cout << "value --" << value << std::endl;
+            stk.pop();
+        }
+    });
+    thread_1.join();
+    thread_2.join();
+    std::cout << stk.top() << std::endl;
+}
+
 int main()
 {
     run_code();
     run_malisious_code();
+    run_trivial_thread_safe_stack();
     return 0;
 }
