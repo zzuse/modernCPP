313a1f6d9da49809d9e327d55c1f7947362026d4
Zhen <zhen@Zhens-MBP.wp.shawcable.net>
2024-12-10 00:00:03 -0600

    condition

---
+++ ConditionVariable/main.cpp
@@ -0,0 +1,51 @@
+#include "common.h"
+#include <iostream>
+#include <list>
+#include <mutex>
+#include <stack>
+#include <thread>
+#include <chrono>
+
+bool have_i_arrived = false;
+int distance_my_destination = 10;
+int distance_coverd = 0;
+
+bool keep_driving() {
+    while(true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        distance_coverd++;
+    }
+    return false;
+}
+
+void keep_awake_all_night() {
+    while (distance_coverd < distance_my_destination) {
+        std::cout << "keep check, whether i am there \n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+    std::cout << "finally i am there, distance_covered = " << distance_coverd << std::endl;
+}
+
+void set_the_alarm_and_take_a_nap() {
+    if(distance_coverd < distance_my_destination) {
+        std::cout << "let me take a nap\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+    }
+    std::cout << "finally i am there, distance_coverd = " << distance_coverd << std::endl;
+}
+
+void run_code() {
+    std::thread driver_thread(keep_driving);
+    std::thread keep_awake_all_night_thread(keep_awake_all_night);
+    std::thread set_the_alarm_and_take_a_nap_thread(set_the_alarm_and_take_a_nap);
+
+    keep_awake_all_night_thread.join();
+    set_the_alarm_and_take_a_nap_thread.join();
+    driver_thread.join();
+}
+
+int main()
+{
+    run_code();
+    return 0;
+}
