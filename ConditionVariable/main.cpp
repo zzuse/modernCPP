0596c58f5cd0c87eefcd4a1fa0c87f08ab040168
zzuse <zzuse@outlook.com>
2024-12-17 23:23:01 -0600

    future policy details

--- ConditionVariable/main.cpp
+++ ConditionVariable/main.cpp
@@ -182,7 +182,11 @@ void run_asnync()
 
 void printing() { std::cout << "printing runs on-" << std::this_thread::get_id() << std::endl; }
 
-int addition(int x, int y) { std::cout << "addition runs on-" << std::this_thread::get_id() << std::endl; }
+int addition(int x, int y)
+{
+    std::cout << "addition runs on-" << std::this_thread::get_id() << std::endl;
+    return x + y;
+}
 
 int substract(int x, int y)
 {
@@ -195,13 +199,15 @@ void run_future()
     std::cout << "main thread id -" << std::this_thread::get_id() << std::endl;
     int x = 100;
     int y = 50;
+    // seperate thread
     std::future<void> f1 = std::async(std::launch::async, printing);
+    // same thread
     std::future<int> f2 = std::async(std::launch::deferred, addition, x, y);
     std::future<int> f3 = std::async(std::launch::deferred | std::launch::async, substract, x, y);
 
     f1.get();
     std::cout << "value recieved using f2 future - " << f2.get() << std::endl;
-    std::cout << "value recieved using f2 future - " << f3.get() << std::endl;
+    std::cout << "value recieved using f3 future - " << f3.get() << std::endl;
 }
 
 int main()
@@ -209,5 +215,6 @@ int main()
     run_code();
     run_thread_safe_queue();
     run_asnync();
+    run_future();
     return 0;
 }
