eb8f2e982a9c6deda6184e3bb492138a2fd645ee
zzuse <zzuse@outlook.com>
2024-12-20 00:21:10 -0600

    reformat

--- ConditionVariable/main.cpp
+++ ConditionVariable/main.cpp
@@ -5,11 +5,11 @@
 #include <iostream>
 #include <list>
 #include <mutex>
+#include <numeric>
 #include <queue>
 #include <stack>
 #include <thread>
 #include <vector>
-#include <numeric>
 
 bool have_i_arrived = false;
 int distance_my_destination = 10;
@@ -213,19 +213,20 @@ void run_future()
 }
 
 int MIN_ELEMENT_COUNT = 1000;
-template<typename iterator>
-int parallel_accumulate(iterator begin, iterator end) {
+template <typename iterator>
+int parallel_accumulate(iterator begin, iterator end)
+{
     long length = std::distance(begin, end);
-    if(length <= MIN_ELEMENT_COUNT)
-    {
+    if (length <= MIN_ELEMENT_COUNT) {
         std::cout << std::this_thread::get_id() << std::endl;
         return std::accumulate(begin, end, 0);
     }
     iterator mid = begin;
-    std::advance(mid, (length+1)/2);
+    std::advance(mid, (length + 1) / 2);
 
     // recursive all to parallel
-    std::future<int> f1 = std::async(std::launch::deferred | std::launch::async, parallel_accumulate<iterator>, mid, end);
+    std::future<int> f1
+        = std::async(std::launch::deferred | std::launch::async, parallel_accumulate<iterator>, mid, end);
     auto sum = parallel_accumulate(begin, mid);
     return sum + f1.get();
 }
