7ef8c652c6fdddaf41a36d41994dba8002af04d1
Zhen <zhen@198-163-235-70.hotspot.bellmts.ca>
2024-12-19 23:39:29 -0600

    parallel accumulate

--- ConditionVariable/main.cpp
+++ ConditionVariable/main.cpp
@@ -8,6 +8,8 @@
 #include <queue>
 #include <stack>
 #include <thread>
+#include <vector>
+#include <numeric>
 
 bool have_i_arrived = false;
 int distance_my_destination = 10;
@@ -208,6 +210,30 @@ void run_future()
     f1.get();
     std::cout << "value recieved using f2 future - " << f2.get() << std::endl;
     std::cout << "value recieved using f3 future - " << f3.get() << std::endl;
+}
+
+int MIN_ELEMENT_COUNT = 1000;
+template<typename iterator>
+int parallel_accumulate(iterator begin, iterator end) {
+    long length = std::distance(begin, end);
+    if(length <= MIN_ELEMENT_COUNT)
+    {
+        std::cout << std::this_thread::get_id() << std::endl;
+        return std::accumulate(begin, end, 0);
+    }
+    iterator mid = begin;
+    std::advance(mid, (length+1)/2);
+
+    // recursive all to parallel
+    std::future<int> f1 = std::async(std::launch::deferred | std::launch::async, parallel_accumulate<iterator>, mid, end);
+    auto sum = parallel_accumulate(begin, mid);
+    return sum + f1.get();
+}
+
+void run_parallel_accumulate_with_async()
+{
+    std::vector<int> v(10000, 1);
+    std::cout << "The sum is " << parallel_accumulate(v.begin(), v.end()) << std::endl;
 }
 
 int main()
