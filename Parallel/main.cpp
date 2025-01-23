4e99de3aa4c9b3f2c6045ea6f46a614b1f45663d
zzuse <zzuse@outlook.com>
2025-01-22 23:56:39 -0600

    partial sum

--- Parallel/main.cpp
+++ Parallel/main.cpp
@@ -373,6 +373,57 @@ void sequential_partial_sum(Iterator first, Iterator last, OutIterator y)
     }
 }
 
+template <typename Iterator>
+void parallel_partial_sum(Iterator first, Iterator last)
+{
+    typedef typename Iterator::value_type value_type;
+
+    struct process_chunk {
+        void operator()(Iterator begin, Iterator last, std::future<value_type>* previous_end_value,
+                        std::promise<value_type>* end_value)
+        {
+            try {
+                Iterator end = last;
+                ++end;
+                std::partial_sum(begin, end, begin);
+                if (previous_end_value) {
+                    auto addend = previous_end_value->get();
+                    *last += addend;
+                    if (end_value) {
+                    }
+                }
+            }
+        }
+    };
+
+    unsigned long const length = std::distance(first, last);
+    if (!length) return;
+
+    unsigned long const min_per_thread = 25;
+    unsigned long const max_thread = (length + min_per_thread - 1) / min_per_thread;
+    unsigned long const hardware_threads = std::thread::hardware_concurrency();
+    unsigned long const num_threads = std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
+    unsigned long const block_size = length / num_threads;
+
+    std::vector<std::thread> threads(num_threads - 1);
+    std::vector<std::promise<value_type>> end_values(num_threads - 1);
+    std::vector<std::future<value_type>> previous_end_values;
+    previous_end_values.reserve(num_threads - 1);
+
+    join_threads joiner(threads);
+
+    Iterator block_start = first;
+    for (unsigned long i = 0; i < (num_threads - 1); ++i) {
+        Iterator block_last = block_start;
+        std::advance(block_last, block_size - 1);
+        threads[i] = std::thread(process_chunk(), block_start, block_last, (i != 0) ? &previous_end_value[i - 1] : 0,
+                                 &end_values[i]);
+        block_start = block_last;
+        ++block_start;
+        previous_end_values.push_back(end_values[i].get_future());
+    }
+}
+
 void sum_ten_nums()
 {
     std::vector<int> ints(10);
--- README.md
+++ README.md
@@ -187,7 +187,8 @@ chapter 43. Parallel quick sort implementation -- commit bd71441fa3d8179085283d4
 chapter 44. parallel for each -- commit 74b6a99de22af256de8eb676259297246d9f8811
 chapter 45. parallel find algorithm -- commit 4386f861b68ee0535b99e8c38756b316f15f2528
 chapter 46. parallel find async -- commit 01a3660594a4825fc41761866927b64d0b246cf1
-chapter 47. Parallel sum algorithm
+chapter 47. Partial sum algorithm -- commit c8e7f7f22e3090272286dfade061a029eea31a13
+chapter 48. Partial sum parallel implementation
 ```
 
 # 现代 C++ 编程实战
