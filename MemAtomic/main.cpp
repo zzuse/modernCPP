6f425e325946eaea763de14fe47eed064f60d8a5
zzuse <zzuse@outlook.com>
2025-02-28 23:55:49 -0600

    compare exchange

--- MemAtomic/main.cpp
+++ MemAtomic/main.cpp
@@ -45,9 +45,21 @@ void run_bool()
     std::cout << "previous value of atomic bool x - " << z << std::endl;
 }
 
+void run_compare_exchange()
+{
+    std::atomic<int> x(20);
+    int expected_value = 20;
+    std::cout << "previous expected_value - " << expected_value << std::endl;
+    bool return_val = x.compare_exchange_weak(expected_value, 6);
+    std::cout << "operation successful   - " << (return_val ? "yes" : "no") << std::endl;
+    std::cout << "current expected_value - " << expected_value << std::endl;
+    std::cout << "current x              - " << x.load() << std::endl;
+}
+
 int main()
 {
     run_flag();
     run_bool();
+    run_compare_exchange();
     return 0;
 }
